26fdd656b9e8dcadd00da3e3c3b650a9bf7c1e8a
Yazan Abdalaziz <Yazan.Abdalaziz@aerdata.com>
2021-11-06 16:43:09 +0200

    add Get route

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// starting point of the application 
+import express from 'express';
+
+const app = express();
+const PORT = 5000;
+app.use(express.json());
+
+// Get Route
+app.get('/', (req, res) =>
+{
+    console.log('get');
+    res.send('Hello World');
+});
+
+app.listen(PORT, () => console.log(`Server is Running on port: http://localhost:${PORT}`));
